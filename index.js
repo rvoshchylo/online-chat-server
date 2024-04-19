48ff40c6e014cfc824dd595b2873d7435aea811b
Ruslan Voshchylo <150591283+rvoshchylo@users.noreply.github.com>
2024-04-19 15:31:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ io.on('connection', (socket) => {
   });
 });  
 
-server.listen(80, () => {
-  console.log('Server is running on port 80');
+server.listen(5001, () => {
+  console.log('Server is running on port 5001');
 });
